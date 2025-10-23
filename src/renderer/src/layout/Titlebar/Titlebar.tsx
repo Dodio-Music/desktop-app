b1aa5a9f00b84808bef5c5baaaa66082ebdf1a3a
Lukas <lanlud21@htl-kaindorf.at>
2025-10-23 17:26:02 +0200

    Added Settings Icon to Titlebar

--- src/renderer/src/layout/Titlebar/Titlebar.tsx
+++ src/renderer/src/layout/Titlebar/Titlebar.tsx
@@ -1,6 +1,7 @@
 import s from "./titlebar.module.css";
 import {VscChromeClose, VscChromeMaximize, VscChromeMinimize, VscChromeRestore} from "react-icons/vsc";
 import {FC, useEffect, useState} from "react";
+import {GoGear} from "react-icons/go";
 
 interface TitlebarProps {
     zoomLevel: number;
@@ -20,6 +21,9 @@ const Titlebar: FC<TitlebarProps> = ({zoomLevel}) => {
             <div className={s.right}>
                 {zoomLevel !== 100 &&
                     <button onClick={() => window.api.resetZoom()} className={s.zoomContainer}><p className={s.zoomLevel}>{zoomLevel}%</p></button>}
+                <div className={s.settingsContainer}>
+                    <GoGear className={s.settings} size={25} />
+                    </div>
                 <div className={s.buttons}>
                     <button onClick={() => window.electron.minimize()}><VscChromeMinimize/></button>
                     <button onClick={() => window.electron.maximize()}>
