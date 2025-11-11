44f9ca680d3bb96625355852fff8e838450505c8
Lukas <lanlud21@htl-kaindorf.at>
2025-11-11 11:10:29 +0100

    Update Settings Icon style && Add cat logo to match playback speed with volume

--- src/renderer/src/pages/Settings/SettingsPage.tsx
+++ src/renderer/src/pages/Settings/SettingsPage.tsx
@@ -1,10 +1,31 @@
-import car from "./catSettings.gif"
+import car from "./car.webm"
 import style from "./SettingsPage.module.css"
+import {useEffect, useRef} from "react";
+import {useSelector} from "react-redux";
+import {RootState} from "@renderer/redux/store";
 
 const SettingsPage = () => {
+
+    const catImg = useRef<HTMLVideoElement>(null);
+    const {volume, isMuted} = useSelector((state: RootState) => state.rendererPlayer);
+
+    useEffect(() => {
+        if (!catImg.current) return;
+        catImg.current.playbackRate =  Math.round(volume * 10) / 10.0 * 2;
+        if (isMuted) catImg.current.pause();
+        if (!isMuted && catImg.current.paused) catImg.current.play();
+        console.log("test")
+    }, [catImg, volume, isMuted]);
+
     return (
         <div className={style.cat}>
-            <img src={car} style={{width: 100, height: 100}} alt=""/>
+            <video
+                src={car}
+                ref={catImg}
+                style={{width: 200}}
+                autoPlay
+                loop
+            />
             <h3>Nothing to see here</h3>
         </div>
     );
