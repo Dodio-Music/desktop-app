615499a64484ccfeab6d46af42645159c8417f18
MasterWoodie <tabletzuhausebeiuns@gmail.com>
2025-12-04 14:36:10 +0100

    add panel resizing

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,11 +8,16 @@ import {useZoom} from "./hooks/useZoom";
 import {useNavigationShortcuts} from "./hooks/useNavigationShortcuts";
 import {Toaster} from "react-hot-toast";
 import {useShortcuts} from "@renderer/hooks/useShortcuts";
+import {ImperativePanelHandle, Panel, PanelGroup, PanelResizeHandle} from "react-resizable-panels";
+import {useRef} from "react";
+
 
 function App() {
     const zoomFactor = useZoom();
     useNavigationShortcuts();
     useShortcuts();
+
+    const leftPanelRef = useRef<ImperativePanelHandle>(null);
 
     return (
         <>
@@ -31,11 +36,21 @@ function App() {
             <div className={s.appContainer} data-theme="purple">
                 <Titlebar zoomLevel={Math.round(zoomFactor * 100)}/>
                 <div className={s.displayContainer}>
-                    <div className={s.mainContainer}>
-                        <Sidebar/>
-                        <ViewBrowser/>
-                        <div className={s.songInfo}></div>
-                    </div>
+                    <PanelGroup className={s.mainContainer} direction={"horizontal"} autoSaveId="mainPanelStructure">
+
+                        <Panel ref={leftPanelRef} collapsible={true} defaultSize={20} minSize={10} maxSize={30}>
+                            <Sidebar/>
+                        </Panel>
+                        <PanelResizeHandle className={s.resizeHandle} />
+                        <Panel>
+                            <ViewBrowser/>
+                        </Panel>
+                        <PanelResizeHandle className={s.resizeHandle} />
+                        <Panel collapsible={true} defaultSize={10} maxSize={20} minSize={8}>
+                            <div className={s.songInfo}></div>
+                        </Panel>
+                    </PanelGroup>
+
                     <PlaybackBar/>
                 </div>
             </div>
